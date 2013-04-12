aac7168899157889bb89404af7e0ee335edc0508
Emmanuel Touzery <etouzery@gmail.com>
2013-04-12 23:21:30 +0200

    tests are looking good

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -19,6 +19,17 @@ main = do
 		it "ok with single element" $ do
 			mergeSuccessiveEvents [eventWithDesc "a" now] `shouldBe` [eventWithDesc "a" now]
 
+		it "does merge" $ do
+			mergeSuccessiveEvents [eventWithDesc "a" now, eventWithDesc "a" now] `shouldBe` [eventWithDesc "a" now]
+
+		it "does not merge too much" $ do
+			mergeSuccessiveEvents [eventWithDesc "a" now, eventWithDesc "b" now]
+				`shouldBe` [eventWithDesc "a" now, eventWithDesc "b" now]
+
+		it "does merge also if titles differ a bit" $ do
+			mergeSuccessiveEvents [eventWithDesc "a (more)" now, eventWithDesc "a (extra)" now]
+				`shouldBe` [eventWithDesc "a (more)" now]
+
 
 eventWithDesc :: T.Text -> UTCTime -> Event
 eventWithDesc descVal now = Event

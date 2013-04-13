6a7c0c2497dbeb8309af012d6431a8d06977991c
Emmanuel Touzery <etouzery@gmail.com>
2013-04-14 00:32:24 +0200

    cleaner GIT testing code

--- tests/GitTests.hs
+++ tests/GitTests.hs
@@ -7,6 +7,7 @@ import Test.HUnit
 import Data.Either
 import Data.Time.LocalTime
 import Data.Time.Calendar
+import qualified Data.Text as T
 
 import Git
 import Util
@@ -16,47 +17,48 @@ runGitTests = do
 	testMerge
 	testCommitWithoutMessage
 
+testGitParseExpect :: T.Text -> Commit -> Assertion
+testGitParseExpect source expected = do
+	case parseCommitsParsec source of
+		(Right (x:[])) -> assertEqual "Parse succeeded, value match" expected x
+		Left pe -> assertBool ("Parse failed" ++ displayErrors pe) False
+		_ -> assertBool "Parse succeeded, wrong results count" False
+
+testMerge :: Spec
 testMerge = it "parses merge commits" $ do
-			let source = "commit b1eec0f4d734432e434385ea83ee5852eaff1d7f\n\
+		let source = "commit b1eec0f4d734432e434385ea83ee5852eaff1d7f\n\
 \Merge: xxxx\n\
 \Author: David <t@a>\n\
 \Date:   Mon Apr 8 18:50:43 2013 +0200\n\
 \\n\
 \ Did merge.\n\
 \\n"
-			let parseResult = parseCommitsParsec source
-			let expected = Commit
-					{
-						commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
-						commitDesc = "Did merge.",
-						commitFiles = [""],
-						commitAuthor = "David <t@a>",
-						commitContents = "<pre></pre>"
-					}
-			case parseResult of
-				(Right (x:[])) -> assertEqual "Parse succeeded, value match" expected x
-				Left pe -> assertBool ("Parse failed" ++ displayErrors pe) False
-				_ -> assertBool "Parse succeeded, wrong results count" False
+		let expected = Commit
+			{
+				commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
+				commitDesc = "Did merge.",
+				commitFiles = [""],
+				commitAuthor = "David <t@a>",
+				commitContents = "<pre></pre>"
+			}
+		testGitParseExpect source expected
 
+testCommitWithoutMessage :: Spec
 testCommitWithoutMessage = it "parses commits without message" $ do
-			let source = "commit b1eec0f4d734432e434385ea83ee5852eaff1d7f\n\
+		let source = "commit b1eec0f4d734432e434385ea83ee5852eaff1d7f\n\
 \Author: David <t@a>\n\
 \Date:   Mon Apr 8 18:50:43 2013 +0200\n\
 \\n\
 \ test/src/main/users.js | 2 ++\n\
 \ 1 file changed, 2 insertions(+)\n\
 \\n"
-			let parseResult = parseCommitsParsec source
-			let expected = Commit
-					{
-						commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
-						commitDesc = "",
-						commitFiles = ["test/src/main/users.js"],
-						commitAuthor = "David <t@a>",
-						commitContents = " test/src/main/users.js | 2 ++\
+		let expected = Commit
+			{
+				commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
+				commitDesc = "",
+				commitFiles = ["test/src/main/users.js"],
+				commitAuthor = "David <t@a>",
+				commitContents = " test/src/main/users.js | 2 ++\
 \ 1 file changed, 2 insertions(+)"
-					}
-			case parseResult of
-				(Right (x:[])) -> assertEqual "Parse succeeded, value match" expected x
-				Left pe -> assertBool ("Parse failed" ++ displayErrors pe) False
-				_ -> assertBool "Parse succeeded, wrong results count" False
+			}
+		testGitParseExpect source expected

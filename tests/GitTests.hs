3aa9ec51b272f01d6b7025dac68fd5d0df289391
Emmanuel Touzery <etouzery@gmail.com>
2013-04-15 22:10:57 +0200

    nicer QQ for multiline strings

--- tests/GitTests.hs
+++ tests/GitTests.hs
@@ -28,14 +28,15 @@ testGitParseExpect source expected = do
 
 testMerge :: Spec
 testMerge = it "parses merge commits" $ do
-		let source = [str|commit b1eec0f4d734432e434385ea83ee5852eaff1d7f
-Merge: xxxx
-Author: David <t@a>
-Date:   Mon Apr 8 18:50:43 2013 +0200
-
- Did merge.
-
-|]
+		let source = [strT|
+				commit b1eec0f4d734432e434385ea83ee5852eaff1d7f
+				Merge: xxxx
+				Author: David <t@a>
+				Date:   Mon Apr 8 18:50:43 2013 +0200
+				
+				 Did merge.
+				
+				|]
 		let expected = Commit
 			{
 				commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
@@ -48,16 +49,17 @@ Date:   Mon Apr 8 18:50:43 2013 +0200
 
 testUsualCommit :: Spec
 testUsualCommit = it "parses usual commits" $ do
-		let source = [str|commit b1eec0f4d734432e434385ea83ee5852eaff1d7f\n\
-Author: David <t@a>
-Date:   Mon Apr 8 18:50:43 2013 +0200
-
- Did commit.
-
- test/src/main/users.js | 2 ++
- 1 file changed, 2 insertions(+)
-
-|]
+		let source = [strT|
+				commit b1eec0f4d734432e434385ea83ee5852eaff1d7f
+				Author: David <t@a>
+				Date:   Mon Apr 8 18:50:43 2013 +0200
+				
+				 Did commit.
+				
+				 test/src/main/users.js | 2 ++
+				 1 file changed, 2 insertions(+)
+				
+				|]
 		let expected = Commit
 			{
 				commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
@@ -70,14 +72,15 @@ Date:   Mon Apr 8 18:50:43 2013 +0200
 
 testCommitWithoutMessage :: Spec
 testCommitWithoutMessage = it "parses commits without message" $ do
-		let source = [str|commit b1eec0f4d734432e434385ea83ee5852eaff1d7f
-Author: David <t@a>
-Date:   Mon Apr 8 18:50:43 2013 +0200
-
- test/src/main/users.js | 2 ++
- 1 file changed, 2 insertions(+)
-
-|]
+		let source = [strT|
+				commit b1eec0f4d734432e434385ea83ee5852eaff1d7f
+				Author: David <t@a>
+				Date:   Mon Apr 8 18:50:43 2013 +0200
+				
+				 test/src/main/users.js | 2 ++
+				 1 file changed, 2 insertions(+)
+				
+				|]
 		let expected = Commit
 			{
 				commitDate = LocalTime (fromGregorian 2013 4 8) (TimeOfDay 18 50 43),
--- tests/Str.hs
+++ tests/Str.hs
@@ -1,8 +1,50 @@
-module Str(str) where
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+
+module Str (strT) where
  
+import Text.ParserCombinators.Parsec
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
- 
-str = QuasiQuoter { quoteExp = stringE }
+import Data.List
 
-strcr = QuasiQuoter { quoteExp = stringE }
+import qualified Data.Text as T
+
+quotedStr :: String -> Q Exp
+quotedStr str = [| T.pack $ manageWhitespace str |]
+
+{-
+  I first expect a carriage return, then on the next line I'll skip
+  all the characters until the first actual text, and then i'll skip
+  the same number of spaces for every line after.
+-}
+manageWhitespace :: String -> String
+manageWhitespace input =
+	let
+		parseResult = parse manageWhitespaceParser "" input 
+	in
+		case parseResult of
+			Left _ -> error "Parse error"
+			Right x -> x
+
+manageWhitespaceParser :: GenParser Char st String
+manageWhitespaceParser = do
+	optional $ string "\r"
+	string "\n"
+	-- check the number of characters until the first
+	-- non-whitespace character.
+	whitespace <- many $ oneOf "\t "
+	let spacesCount = length whitespace
+	firstLine <- many $ noneOf "\r\n"
+	many $ oneOf "\r\n"
+	otherLines <- many $ lineSkipSpaces spacesCount
+	return $ intercalate "\n" (firstLine:otherLines)
+
+lineSkipSpaces :: Int -> GenParser Char st String
+lineSkipSpaces spaces = do
+	count spaces (oneOf "\t ")
+	result <- many $ noneOf "\r\n"
+	many $ oneOf "\r\n"
+	return result
+
+strT :: QuasiQuoter
+strT = QuasiQuoter quotedStr undefined undefined undefined

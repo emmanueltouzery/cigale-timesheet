6adaf56d5cd1fbe569d32fb725956534baacaf08
Emmanuel Touzery <etouzery@gmail.com>
2013-06-11 21:32:36 +0200

    start on elm

--- src/App/Main.hs
+++ src/App/Main.hs
@@ -9,7 +9,14 @@ import Control.Monad.IO.Class
 import qualified Data.Text.Encoding as TE
 import System.Directory
 import System.IO
-import Data.ByteString.Lazy (hPut)
+import Data.ByteString.Lazy as LBS (hPut, fromChunks, concat)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy.Char8 as DBLC
+import qualified Language.Elm as Elm
+import qualified Data.Text as T
+import Data.Maybe (fromJust)
+import Text.Blaze.Html5
+import Text.Blaze.Html.Renderer.String
 
 import qualified Timesheet
 import qualified Settings
@@ -25,26 +32,42 @@ site =
             ("configdesc", configdesc),
             ("config", serveFile "config.html"),
             ("configVal", configVal),
-            ("configUpdate", configUpdate)
+            ("configUpdate", configUpdate),
+            ("elm/:filename", serveElm)
           ] <|>
     dir "static" (serveDirectory ".")
 
 timesheet :: Snap ()
 timesheet = do
+    modifyResponse $ setContentType "application/json"
     setTimeout 3600
     param <- getParam "tsparam"
     maybe (writeBS "must specify the month and year in URL, like so: /timesheet/2012-11")
           handleTimesheet param
+
+serveElm :: Snap ()
+serveElm = do
+	paramFilename <- getParam "filename"
+	let elmFilename = T.unpack $ TE.decodeUtf8 $ fromJust paramFilename
+	let filename = "/home/emmanuel/Dropbox/haskell/timesheet/" ++ elmFilename ++ ".elm"
+	--let filename = elmFilename ++ ".elm"
+	elmSource <- liftIO $ readFile $ filename
+	let elmJsLoc = "/static/elm-runtime.js"
+	let compiled = docTypeHtml $ Elm.toHtml elmJsLoc "Timesheet" elmSource
+	writeLBS $ DBLC.pack $ renderHtml compiled
 
 handleTimesheet param = do
 	jsonData <- liftIO $ Timesheet.process $ TE.decodeUtf8 param
 	writeLBS jsonData
 
 configdesc :: Snap ()
-configdesc = writeLBS Timesheet.getEventProvidersConfig
+configdesc = do
+	modifyResponse $ setContentType "application/json"
+	writeLBS Timesheet.getEventProvidersConfig
 
 configVal :: Snap ()
 configVal = do
+	modifyResponse $ setContentType "application/json"
 	settingsFile <- liftIO Config.getConfigFileName
 	serveFile $ settingsFile
 

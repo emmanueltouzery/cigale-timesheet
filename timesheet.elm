8079b92e6be2baca0c4e0b6577f62b90eb471553
Emmanuel Touzery <etouzery@gmail.com>
2013-06-15 18:06:16 +0200

    now works, but performance..

--- timesheet.elm
+++ timesheet.elm
@@ -3,18 +3,19 @@ import Json
 
 -- do i really have an array, or is it maybe a hash?
 serverJson : Signal [JsonValue]
-serverJson = let jsonTxtSig = lift respStr (Http.sendGet (constant "/timesheet/2013-06-01"))
+serverJson = let jsonTxtSig = lift respStr (Http.sendGet (constant "/timesheet/2013-06-07"))
              in lift getJsonArray jsonTxtSig
 
 getJsonArray : String -> [JsonValue]
 getJsonArray jsonTxt = case Json.fromString jsonTxt of
-                        Just x -> x
-                        _ -> [Number 1, Number 2, Number 3]
+                        Just (Array x) -> x
+                        _ -> []
 
 respStr : Response String -> String
 respStr resp = case resp of
     Success string -> string
-    _ -> ""
+    Waiting -> "[1,2]"
+    _ -> "[1]"
 
 showResults : String -> Element
 showResults json = flow down [plainText "Hour", plainText json, plainText "Description", plainText "Extra info"]

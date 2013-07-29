c801256d06cc21f3f60e5529ab92de3104bca5e6
Emmanuel Touzery <etouzery@gmail.com>
2013-07-29 21:36:13 +0200

    format date. more JS...

--- src/App2/FayApp.hs
+++ src/App2/FayApp.hs
@@ -25,12 +25,16 @@ processResults events = mapM_ processResult events
 		processResult event = eventsTable >>= (append $ makeEventRow event)
 
 makeEventRow :: Event -> String
-makeEventRow event = makeTableRow $ map ($ event) [eventDate, pluginName, desc, extraInfo]
+makeEventRow event = makeTableRow $ map ($ event) [\x -> formatTime $ eventDate x,
+							pluginName, desc, extraInfo]
 
 makeTableRow :: [String] -> String
 makeTableRow cols = "<tr><td>" ++ intercalate "</td><td>" cols ++ "</td></tr>"
 
 data JQuery
+
+formatTime :: String -> String
+formatTime = ffi "formatTime(%1)"
 
 ajax :: String -> (Automatic b -> Fay ()) -> Fay ()
 ajax = ffi "jQuery.ajax(%1, {'type': 'GET', contentType: 'text/json', processData: false, 'success' : %2 })"

a2888464e5924c143430b22906470355a394ef69
Emmanuel Touzery <etouzery@gmail.com>
2017-07-01 20:54:43 +0200

    slack: now also fetching IMs

--- src/EventProviders/Slack.hs
+++ src/EventProviders/Slack.hs
@@ -18,6 +18,7 @@ import Web.Slack.Common as Slack
 import Web.Slack.User as Slack
 import Web.Slack.Group as Slack
 import Web.Slack.Channel as Slack
+import Web.Slack.Im as Slack
 
 import TsEvent
 import qualified Util
@@ -52,62 +53,63 @@ getSlackMessages (SlackConfigRecord token) _ date _ = do
     let slackRun = fmap reduceErrors . Slack.run manager
     users <- ExceptT $ slackRun (Slack.usersList token)
     let userIdToName = Map.fromList $ (userId &&& userName) <$> listRspMembers users
-    let getMsgs = getMessages slackRun token date userIdToName
+    let getUserName = fromMaybe "Unknown user" . flip Map.lookup userIdToName
+    let getMsgs = getMessages slackRun token date getUserName
     groupEvts <- getMsgs Slack.groupsList listRspGroups groupsHistory groupId groupName
     channelEvts <- getMsgs (`Slack.channelsList` mkListReq) listRspChannels channelsHistory channelId channelName
-    return (groupEvts ++ channelEvts)
+    imsEvts <- getMsgs Slack.imList listRspIms imHistory imId (getUserName . imUser)
+    return (groupEvts ++ channelEvts ++ imsEvts)
 
 type SlackRun = forall a. (ClientM (Response a) -> IO (Either String a))
 
 getMessages :: SlackRun
             -> Text
             -> Day
-            -> Map UserId Text
+            -> (UserId -> Text)
             -> (Text -> ClientM (Response chatlist))
             -> (chatlist -> [chat])
             -> (Text -> HistoryReq -> ClientM (Response HistoryRsp))
             -> (chat -> Text)
             -> (chat -> Text)
             -> ExceptT String IO [TsEvent]
-getMessages slackRun token date userIdToName getList listGetChats getHistory getChatId getChatName = do
+getMessages slackRun token date getUserName getList listGetChats getHistory getChatId getChatName = do
     groups <- lift $ slackRun (getList token)
     case groups of
       Left er    -> throwE er
       Right grps -> ExceptT $ fmap (fmap catMaybes . sequence) <$>
-          traverse (fetchMessages token slackRun date userIdToName getHistory getChatId getChatName) $ listGetChats grps
+          traverse (fetchMessages token slackRun date getUserName getHistory getChatId getChatName) $ listGetChats grps
 
 fetchMessages :: Text
               -> SlackRun
               -> Day
-              -> Map UserId Text
+              -> (UserId -> Text)
               -> (Text -> HistoryReq -> ClientM (Response HistoryRsp))
               -> (a -> Text)
               -> (a -> Text)
               -> a
               -> IO (Either String (Maybe TsEvent))
-fetchMessages token slackRun date userIdToName fetcher convId convName item = do
+fetchMessages token slackRun date getUserName fetcher convId convName item = do
     historyRsp <- slackRun $ Slack.historyFetchAll token fetcher
                (convId item) messagesFetchBy
                (mkSlackTimestamp $ UTCTime date 0)
                (mkSlackTimestamp $ UTCTime (addDays 1 date) 0)
-    return $ fmapR (messagesToEvent (convName item) userIdToName . historyRspMessages) historyRsp
+    return $ fmapR (messagesToEvent (convName item) getUserName . historyRspMessages) historyRsp
 
-messagesToEvent :: Text -> Map UserId Text -> [Slack.Message] -> Maybe TsEvent
+messagesToEvent :: Text -> (UserId -> Text) -> [Slack.Message] -> Maybe TsEvent
 messagesToEvent _ _ [] = Nothing
-messagesToEvent channel userIdToName msgs@(msg:_) = Just TsEvent
+messagesToEvent channel getUserName msgs@(msg:_) = Just TsEvent
     {
         pluginName   = getModuleName getSlackProvider,
         eventIcon    = "glyphicons-245-conversation",
         eventDate    = slackTimestampTime (messageTs msg),
         desc         = channel,
         extraInfo    = "",
-        fullContents = Just (formatMessages userIdToName msgs)
+        fullContents = Just (formatMessages getUserName msgs)
     }
 
-formatMessages :: Map UserId Text -> [Slack.Message] -> Text
-formatMessages userIdToName msgs = T.intercalate "<br/>" (formatMessage <$> reverse msgsWithUser)
+formatMessages :: (UserId -> Text) -> [Slack.Message] -> Text
+formatMessages getUserName msgs = T.intercalate "<br/>" (formatMessage <$> reverse msgsWithUser)
     where
       msgsWithUser = filter (isJust . messageUser) msgs
-      getUserName = fromMaybe "Unknown user" . flip Map.lookup userIdToName
       formatMessage Message{..} = Util.linksForceNewWindow $
           T.concat ["<b>", getUserName (fromJust messageUser), ":</b> ", messageText]
